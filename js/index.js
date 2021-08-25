98fbd84338eb1a3825643834237a14a2345625d1
miyakooti <niimaru09@gmail.com>
2021-08-25 22:32:16 +0900

    apiから帰ってきたデータをDOMで反映させるサンプル

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-window.addEventListener("load", function() {
-    console.log("hogehoge");
-})
+const lists = document.getElementById("lists");
+
+window.addEventListener("load", async function () {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    users.forEach(function (user) {
+
+        // <li></li>をつくる
+        const list = document.createElement("li");
+        list.innerText = user.name;
+
+        // それをhtmlファイルのulタグの中にぶちこむ
+        lists.appendChild(list);
+    });
+});
